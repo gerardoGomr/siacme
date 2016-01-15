3909dafe46d5e8479e73d70dce86f498fcf814a8
Gerardo Adrián Gómez Ruiz <gerardo.gomr@gmail.com>
2016-01-14 22:26:33 -0600

    Descargué Barry Laravel IDE Helper

--- config/app.php
+++ config/app.php
@@ -137,6 +137,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Illuminate\Html\HtmlServiceProvider::class,
+        Elibyy\TCPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
